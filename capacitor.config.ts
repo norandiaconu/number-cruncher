783f81de4eea385d0f2e73c0711a0eb866ad5b4a
Noran Diaconu <norandiaconu@gmail.com>
2023-08-18 17:05:47 -0400

    Changed Android build
    Reduced package size

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,13 @@
 import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-    appId: "com.norandiaconu.numbercruncher",
-    appName: "Number Cruncher",
-    webDir: "docs",
-    bundledWebRuntime: false,
-    backgroundColor: "#000000",
+  appId: "com.norandiaconu.numbercruncher",
+  appName: "Number Cruncher",
+  webDir: "docs",
+  server: {
+    androidScheme: "https",
+  },
+  backgroundColor: "#000000",
 };
 
 export default config;
