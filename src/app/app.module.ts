d8729a9879f44b15e918a789f8c175e17f971435
Noran Diaconu <norandiaconu@gmail.com>
2025-04-21 21:42:37 -0400

    Updated to Angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,9 @@
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoadService } from './load.service';
 
-@NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, HttpClientModule],
-    providers: [LoadService],
-    bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule], providers: [LoadService, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
